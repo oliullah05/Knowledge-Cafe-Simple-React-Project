8efb4fa5083f0d1494fb9af682b6387e14650473
oliullah05 <mdolihasan@gmail.com>
2023-03-30 21:45:12 +0600

    Header section done

---
+++ src/Component/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../assets/45684685.jpg"
+
+const Header = () => {
+    return (
+        <div className='flex justify-between bg-blue-300'>
+           <h1 className='text-3xl font-bold'>OLi's Knowledge CAfe</h1>
+           <div><img className='' src="../assets/45684685.jpg" alt="" /></div> 
+        </div>
+    );
+};
+
+export default Header;
